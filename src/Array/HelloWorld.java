3783f693977aa206ed37c29b7932fb61760bc29c
Thien Nguyen <nguyenhaothien100101@gmail.com>
2025-05-11 09:25:04 +0700

    feat(variable): uodate oop

---
+++ src/Array/HelloWorld.java
@@ -0,0 +1,20 @@
+package Array;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("D:\\Java\\re_f00\\src\\input.txt"));
+        PrintWriter pw = new PrintWriter(new File("D:\\Java\\re_f00\\src\\output.txt"));
+        int sumValue = 0;
+        while(sc.hasNext()) {
+            sumValue += sc.nextInt();
+        }
+        pw.println(sumValue);
+
+        pw.close();
+    }
+}
---
+++ src/OOP/oop.java
@@ -0,0 +1,7 @@
+package OOP;
+
+public class oop {
+    public static void main(String[] args) {
+
+    }
+}
